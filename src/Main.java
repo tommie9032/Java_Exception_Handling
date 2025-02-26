e65f4a6fe9d3b0c5ac84f8fa7721ebc5a9db84cb
tommie9032 <tomthomas9032@gmail.com>
2025-02-26 11:30:14 +0530

    Main program 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        int a = 2;
-        int b = 0;
-
-        try{
-            System.out.println("Division is :"+a/b);
-
+    public static void checkAge(int age) throws AgeException{
+        if(age<18){
+            throw new AgeException("Age less than 18!!!");
         }
-        catch(ArithmeticException e){
-            System.out.println("Denominator is 0!!!!");
+        else {
+            System.out.println("Accept!!!");
+        }
+    }
+    public static void main(String[] args) {
+        int age = 12;
+        try{
+            checkAge(age);
+        } catch (AgeException e) {
+            System.out.println(e);
         }
     }
 }
